fb9415beac1312509e8cfb16ae619270f8d9321a
Andres Ruales <danielandresruales@hotmail.com>
2023-05-06 14:02:57 -0500

    Listo los botones encriptar y desencriptar funcionando con su logica

--- script.js
+++ script.js
@@ -1,13 +1,116 @@
-/*PARA CAPTURAR LO DEL TEXAREA*/
-const textArea = document.querySelector
+/*PARA CAPTURAR LO DEL usuario del textarea*/
+const word = document.querySelector(".inputTexto")
+const mensajeEncriptado = document.querySelector(".mensajeEncriptado")
 
 
 
 
+function btnEncriptar(){
+    const textoEncriptado = encriptar(word.value);
+    mensajeEncriptado.value = textoEncriptado;
+    word.value="";
+    mensajeEncriptado.style.backgroundImage="none"
+}
+
+function btnDesencriptar(){
+    const textoEncriptado = desencriptar(word.value);
+    mensajeEncriptado.value = textoEncriptado;
+    word.value="";
+    mensajeEncriptado.style.backgroundImage="none"
+}
+
+function btnCopiar(){
+    const textoCopiado = document.getElementById(mensajeEncriptado).value;
+    navigator.clipboard.writeText(textoCopiado);
+}
 
 
+/*Diccionario*/
 
+const diccionario = {
+    'a': 'ai',
+    'e': 'enter',
+    'i': 'imes',
+    'o': 'ober',
+    'u': 'ufat'
+  };
+ 
+/*Funcion para encriptar*/
+function encriptar(word) {
+    let palabraAencriptar = '';
+    for (let i = 0; i < word.length; i++) {
+      const letter = word[i].toLowerCase();
+      palabraAencriptar += diccionario[letter] || letter;
+    }
+    return palabraAencriptar;
+  }
+  
 
-
-
+/*Funcion para Desencriptar*/
+function desencriptar(word) {
+    let palabraDesencriptada = '';
+    for (let i = 0; i < word.length;i++) {
+      let minuscula=word[i].toLowerCase();
+      if(minuscula == "a"){
+        let palabra = "";
+        for(let j =i; j< word.length; j++) {
+          palabra +=  word[j].toLowerCase();
+          if(palabra == "ai"){
+            palabraDesencriptada+= "a";
+            i=j;
+            break;
+          }
+        }
+      }
+      else if (minuscula == "e"){
+        let palabra = "";
+        for(let j =i; j< word.length; j++) {
+          palabra +=  word[j].toLowerCase();
+          if(palabra == "enter"){
+            palabraDesencriptada+= "e";
+            i=j;
+            break;
+          }
+        }
+      }
+      else if (minuscula == "i"){
+        let palabra = "";
+        for(let j =i; j< word.length; j++) {
+          palabra +=  word[j].toLowerCase();
+          if(palabra == "imes"){
+            palabraDesencriptada+= "i";
+            i=j;
+            break;
+          }
+        }
+        
+      }
+      else if (minuscula == "o"){
+        let palabra = "";
+        for(let j =i; j< word.length; j++) {
+          palabra +=  word[j].toLowerCase();
+          if(palabra == "ober"){
+            palabraDesencriptada+= "o";
+            i=j;
+            break;
+          }
+        }
+      }
+      else if (minuscula == "u"){
+        let palabra = "";
+        for(let j =i; j< word.length; j++) {
+          palabra +=  word[j].toLowerCase();
+          if(palabra == "ufat"){
+            palabraDesencriptada+= "u";
+            i=j;
+            break;
+          }
+        }
+      }
+      else{
+        palabraDesencriptada+=minuscula
+      }
+    }
+    return palabraDesencriptada;
+  }
 
